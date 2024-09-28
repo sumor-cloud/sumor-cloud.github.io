bea12de8a23e8e54dffefcf132e44c8eed93e213
ohnow <dennis@sumor.com>
2024-09-28 22:44:16 +0800

    dev

--- build/entry/generateStatus.js
+++ build/entry/generateStatus.js
@@ -6,10 +6,13 @@ export default async language => {
 
   let markdown = ''
   const packagesStatus = packages.map(pkg => {
-    const status = `[![CI](https://github.com/sumor-cloud/${pkg}/actions/workflows/ci.yml/badge.svg)](https://github.com/sumor-cloud/${pkg}/actions/workflows/ci.yml)
-[![Test](https://github.com/sumor-cloud/${pkg}/actions/workflows/ut.yml/badge.svg)](https://github.com/sumor-cloud/${pkg}/actions/workflows/ut.yml)
-[![Coverage](https://github.com/sumor-cloud/${pkg}/actions/workflows/coverage.yml/badge.svg)](https://github.com/sumor-cloud/${pkg}/actions/workflows/coverage.yml)
-[![Audit](https://github.com/sumor-cloud/${pkg}/actions/workflows/audit.yml/badge.svg)](https://github.com/sumor-cloud/${pkg}/actions/workflows/audit.yml)`
+    const status = `
+[![NPM Version](https://img.shields.io/npm/v/@sumor/${pkg}?logo=npm&label=NPM)](https://www.npmjs.com/package/@sumor/${pkg})
+[![NPM Downloads](https://img.shields.io/npm/dw/@sumor/${pkg}?logo=npm&label=Downloads)](https://www.npmjs.com/package/@sumor/${pkg})
+[![GitHub CI](https://img.shields.io/github/actions/workflow/status/sumor-cloud/${pkg}/ci.yml?logo=github&label=CI)](https://github.com/sumor-cloud/${pkg}/actions/workflows/ci.yml)
+[![GitHub Test](https://img.shields.io/github/actions/workflow/status/sumor-cloud/${pkg}/ut.yml?logo=github&label=Test)](https://github.com/sumor-cloud/${pkg}/actions/workflows/ut.yml)
+[![GitHub Coverage](https://img.shields.io/github/actions/workflow/status/sumor-cloud/${pkg}/coverage.yml?logo=github&label=Coverage)](https://github.com/sumor-cloud/${pkg}/actions/workflows/coverage.yml)
+[![GitHub Audit](https://img.shields.io/github/actions/workflow/status/sumor-cloud/${pkg}/audit.yml?logo=github&label=Audit)](https://github.com/sumor-cloud/${pkg}/actions/workflows/audit.yml)`
 
     return `## [@sumor/${pkg}](${language === 'en' ? '' : `/${language}`}/${pkg})\n${status}\n`
   })
